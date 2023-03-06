7f7de28e38ea493a9c31aa88d22eb5bb6786cdf2
Mike Bybee <me@mikebybee.com>
2023-03-05 20:37:41 -0600

    Delete consts.js

--- src/consts.js
+++
@@ -1,5 +0,0 @@
-import * as FileSystem from "expo-file-system"
-
-export const IMAGE_CACHE_FOLDER = `${FileSystem.cacheDirectory}`
-// export const IMAGE_CACHE_FOLDER = `${FileSystem.documentDirectory}images/`
-export default IMAGE_CACHE_FOLDER
